3ee38614e8315d9dd82335152e6c2c15172ddefc
riku534 <opa9788@gmail.com>
2024-06-29 07:32:01 +0900

    j

--- server/domain/novel/useCase/novelUseCase.ts
+++ server/domain/novel/useCase/novelUseCase.ts
@@ -1,13 +1,12 @@
-import { parseDocument } from 'htmlparser2';
+import { load } from 'cheerio';
+import * as iconv from 'iconv-lite';
 import { transaction } from 'service/prismaClient';
-import {load }  from 'cheerio';
-import * as iconv from "iconv-lite";
 
 export const novelUseCase = {
   scrape: (aozoraUrl: string): Promise<string> =>
     transaction('RepeatableRead', async (tx) => {
       const html = await fetch(aozoraUrl).then((b) => b.text());
-      const $ = load(iconv.decode(Buffer.from(html) , "Shift_JIS"));
+      const $ = load(iconv.decode(Buffer.from(html), 'Shift_JIS'));
       const body = $('body');
 
       return body.text();
